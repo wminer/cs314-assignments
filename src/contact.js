33ec7851b447898c1d9c2e35f9f4fac95d28c0c6
willminer <wminer@zagmail.gonzaga.edu>
2018-05-08 00:01:25 -0700

    12:01 am isn't too late right?

--- src/contact.js
+++ src/contact.js
@@ -1,1 +1,37 @@
 'use strict';
+// let submitbtn = document.getElementById("submit");
+// function validateData() {
+//   console.log("ENTERING VALIDATE DATA");
+//   let inpContainer = document.getElementById("inputs-container");
+//   let name = document.getElementById("fullname");
+//   console.log(name);
+//   let email = document.getElementById("email");
+//   console.log(email);
+//   let phone = document.getElementById("phone");
+//   let message = document.getElementById("textbox");
+//   let contactDiv = document.getElementById("contact-div");
+//   if((email.innerHTML.includes("@") && email.innerHTML.includes(".com"))) {
+//     contactDiv.innerHTML = "Please enter a valid email. You entered "+ email.innerHTML;
+//   } else {
+//     contactDiv.innerHTML = "Type your message below";
+//     for(let i = 0;i < 4;i++) {
+//       let p_elem = document.createElement("p");
+//       if(i == 0) {
+//         p_elem.innerHTML = "Name: "+name.value;
+//         console.log(name.value);
+//       } else if(i==1) {
+//         p_elem.innerHTML = "Email: "+email.value;
+//         console.log(email.value);
+//       } else if(i==2) {
+//         p_elem.innerHTML = "Phone: "+phone.value;
+//         console.log(phone.value);
+//       } else if(i==3) {
+//         p_elem.innerHTML = "Message: "+message.value;
+//         console.log(message.value);
+//       }
+//       inpContainer.appendChild(p_elem);
+//     }
+//   }
+// }
+//
+// submitbtn.onclick = validateData;
--- src/maps.js
+++ src/maps.js
@@ -1,8 +1,182 @@
 'use strict';
-var map;
+let map;
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.397, lng: 150.644},
-      zoom: 7
+      center: { lat: 39.8283459, lng: -98.5794797},
+      zoom: 4.3,
+      styles: [
+    {
+        "stylers": [
+            {
+                "hue": "#bbff00"
+            },
+            {
+                "weight": 0.5
+            },
+            {
+                "gamma": 0.5
+            }
+        ]
+    },
+    {
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.natural",
+        "stylers": [
+            {
+                "color": "#a4cc48"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "visibility": "on"
+            },
+            {
+                "weight": 1
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "gamma": 1.14
+            },
+            {
+                "saturation": -18
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "saturation": 30
+            },
+            {
+                "gamma": 0.76
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "weight": 0.4
+            },
+            {
+                "lightness": -8
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#4aaecc"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "stylers": [
+            {
+                "color": "#718e32"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "stylers": [
+            {
+                "saturation": 68
+            },
+            {
+                "lightness": -61
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "weight": 2.7
+            },
+            {
+                "color": "#f4f9e8"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway.controlled_access",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "weight": 1.5
+            },
+            {
+                "color": "#e53013"
+            },
+            {
+                "saturation": -42
+            },
+            {
+                "lightness": 28
+            }
+        ]
+    }
+]
     });
+
+    let marker = new google.maps.Marker({
+      position: { lat: 47.6129432 , lng: -122.4821475 },
+      map: map,
+      title: "My Birthplace, Seattle"
+    });
+
+    marker = new google.maps.Marker({
+      position: { lat: 42.8215174, lng: -106.4649636 },
+      map: map,
+      title: "My Mom's Birthplace, Casper"
+    });
+
+    marker = new google.maps.Marker({
+      position: { lat: 42.3140089, lng: -71.2504676 },
+      map: map,
+      title: "My Dad's Birthplace, Boston"
+    });
+
+
+
   }
